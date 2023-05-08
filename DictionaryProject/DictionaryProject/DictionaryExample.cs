379643284beaf3082aa84627361b57d003ec9db1
ronitshinde <rony.shinde753@gmail.com>
2023-05-08 09:17:23 +0530

    [Ronit] Add : UC1-Code to Remove and clear the dicitionary

---
+++ DictionaryProject/DictionaryProject/DictionaryExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryProject
+{
+    public class DictionaryExample
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        public void DictionaryMethod()
+        {
+            dictionary.Add("1", "Ronnie");
+            dictionary.Add("2", "Messi");
+            dictionary.Add("3", "Ronaldo");
+            dictionary.Add("4", "Neymar");
+            Console.WriteLine(dictionary["1"]);
+            Console.WriteLine(dictionary["2"]);
+            dictionary["5"] = "Ronaldinho";
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                Console.WriteLine("{0} : {1}", kvp.Key, kvp.Value);
+            }
+            Dictionary<int, string> keyValue = new Dictionary<int, string>();
+            keyValue.Add(1, "Ambani");
+            keyValue.Add(2, "Birla");
+            keyValue.Add(3, "Mahindra");
+            //Remove the value
+            keyValue.Remove(2);
+            foreach (KeyValuePair<int, string> pair in keyValue)
+            {
+                Console.WriteLine("{0} : {1}", pair.Key, pair.Value);
+            }
+            if (keyValue.ContainsKey(3))
+            {
+                keyValue[3] = "Tata";
+            }
+            foreach (KeyValuePair<int, string> pair in keyValue)
+            {
+                Console.WriteLine("{0} : {1}", pair.Key, pair.Value);
+            }
+            //Clear method is used to clear the dictionary.
+            keyValue.Clear();
+            foreach (KeyValuePair<int, string> pair in keyValue)
+            {
+                Console.WriteLine("{0} : {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+}
+
---
+++ DictionaryProject/DictionaryProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryExample dictionary = new DictionaryExample();
+            dictionary.DictionaryMethod();
+            Console.ReadLine();
+        }
+    }
+}
